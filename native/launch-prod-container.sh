c44b83e1449e5250193e0dd9f5a85a23e47880cf
Dan Tidwell <dant@davis-laptop.local>
2016-08-18 16:12:05 -0500

    get static linking working so container can be built from scratch

--- native/launch-prod-container.sh
+++ native/launch-prod-container.sh
@@ -1,3 +1,12 @@
-rm -rf bin && \
-mkdir -p bin && \ 
-gcc native-app.c -o bin\native-app
+#clean up any containers/images by the same name
+docker kill native-app-prod 2> /dev/null
+docker rm native-app-prod 2> /dev/null
+docker rmi native-app-prod 2> /dev/null
+
+rm -rf ./bin && \
+mkdir -p ./bin && \
+gcc -static native-app.c -o native-app && \
+mv native-app ./bin/native-app
+
+docker build -f ./Dockerfile-prod --no-cache=true -t native-app-prod ./
+docker run --name native-app-prod native-app-prod
