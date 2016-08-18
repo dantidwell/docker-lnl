61c6226df426f942b7f59f38028ec9ad359061cc
Dan Tidwell <dant@davis-laptop.local>
2016-08-18 13:59:02 -0500

    add container and image clean up prior to build and run

--- node/launch-container.sh
+++ node/launch-container.sh
@@ -1,2 +1,7 @@
-docker build --no-cache -t node-app ./ && \
+#clean up any containers/images by the same name
+docker kill node-app && \
+docker rm node-app && \
+docker rmi node-app && \
+#build the image and then launch a container 
+docker build --no-cache=true -t node-app ./ && \
 docker run -d -e NODE_ENV=docker -p 3000:80 --name node-app node-app
