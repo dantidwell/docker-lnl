2423304241f7d61b749bdbeba971562ebdb9ef1d
Dan Tidwell <dant@davis-laptop.att.net>
2016-08-18 22:12:16 -0500

    add some comments to the dockerfile, redirect stderr to /dev/null

--- node/launch-container.sh
+++ node/launch-container.sh
@@ -1,7 +1,7 @@
 #clean up any containers/images by the same name
-docker kill node-app && \
-docker rm node-app && \
-docker rmi node-app && \
+docker kill node-app 2> /dev/null
+docker rm node-app 2> /dev/null
+docker rmi node-app 2> /dev/null
 #build the image and then launch a container 
 docker build --no-cache=true -t node-app ./ && \
 docker run -d -e NODE_ENV=docker -p 3000:80 --name node-app node-app
